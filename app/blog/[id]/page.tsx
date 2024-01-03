881b978dab6aa45198db6ea2d46d46b79d2112cc
kolade ore <koladeore@gmail.com>
2024-01-03 02:50:40 +0100

    update

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -5,39 +5,48 @@ import {
   AiOutlineInstagram,
   AiOutlineFacebook,
 } from "react-icons/ai";
-interface searchParamsTypes {
-  id: string;
-  title: string;
-  image_path: string;
-  paragraph: string;
-  featured: boolean;
-  topPost: boolean;
-  tags: string;
-  authorImage: string;
-  authorName: string;
-  publishDate: string;
-}
-const page = ({ searchParams }: {searchParams: searchParamsTypes}) => {
-  const post = searchParams  
+import { PostTypes } from "@/types/postTypes";
+import { formatDate } from "@/utils/formateDate";
+
+const getData = async (id: string) => {
+  const res = await fetch(
+    `http://localhost:3000/api/post/${id}`,
+    {
+      cache: "no-store",
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed");
+  }
+
+  return res.json();
+};
+
+const page = async ({ params }: { params: PostTypes }) => {
+  const { id } = params;
+  const post = await getData(id);
   return (
     <div className="w-[95%] mx-auto max-w-[1450px]">
       <div className="w-full h-[400px] relative mb-5">
         <Image
           fill
           alt="image for blog"
-          src={post.image_path}
+          src={post.img}
           className="object-cover"
         />
       </div>
 
-      <Tag text={post.tags.toString()} />
+      <Tag text={post.category} />
       <h2 className="text-4xl font-extrabold uppercase text-tertiary my-3">
         {post.title}
       </h2>
 
       <div className="flex md:gap-20 gap-5 relative mt-10 md:flex-row flex-col">
         <aside
-          className="md:sticky md:top-3/4 md:h-screen"
+          className="md:sticky
+        md:top-3/4 md:h-screen
+        "
         >
           <span className="uppercase text-2xl font-extrabold text-tertiary">
             Share:
@@ -50,33 +59,25 @@ const page = ({ searchParams }: {searchParams: searchParamsTypes}) => {
         </aside>
 
         <article>
-          <p className="text-xl">
-            {post.paragraph}
-            {post.paragraph}
-            {post.paragraph}
-            {post.paragraph}
-            {post.paragraph}
-            {post.paragraph}
-            {post.paragraph}
-          </p>
+          <p className="text-xl">{post.desc}</p>
 
           <div className="mt-5 flex gap-5 items-center">
             <Image
-              src={post.authorImage}
+              src={post.user.image}
               width={500}
               height={500}
               alt={`Image of ${post.authorName}`}
               className="rounded-full w-20 h-20 object-cover"
             />
             <div className="flex gap-1 flex-col">
-              <span>{post.authorName}</span>
-              <span>{post.publishDate}</span>
+              <span>{post.user.name}</span>
+              <span>{formatDate(post.createdAt)}</span>
             </div>
           </div>
         </article>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
